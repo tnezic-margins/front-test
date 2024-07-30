51dc46ca719e295575a41fcf8ae1423d03c9dfa0
tnezic-margins <tihor.nezic@valeremargins.com>
2024-07-30 08:05:13 +0200

    Remove old file import

--- src/features/recipes/index.ts
+++ src/features/recipes/index.ts
@@ -1,4 +1,3 @@
 export { RecipesList } from "./ui/elements/RecipesList/RecipesList";
 export { RecipesItem } from "./ui/elements/RecipesItem/RecipesItem";
-export { GetSingleRecipeButton } from "./ui/elements/GetSingleRecipeButton/GetSingleRecipeButton";
 export { RandomRecipe } from "./ui/elements/RandomRecipe/RandomRecipe";
